f1ce07b5ba11e637f4893a8e67478e497eb35ff7
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-09-15 21:07:26 +0530

    practice:singly linked list prependNode

--- Singly Linked List/c++/singly_linked_list.cpp
+++ Singly Linked List/c++/singly_linked_list.cpp
@@ -2,98 +2,75 @@
 
 using namespace std;
 
-class Node
-{
-public:
-	int data;
-	Node *next;
-
-	Node()
-	{
-		data = 0;
-		next = NULL;
-	}
-
-	Node(int data)
-	{
-		this->data = data;
-		this->next = NULL;
-	}
+class Node {
+ public:
+  int data;
+  Node* next;
+  Node(int val) : data(val), next(NULL) {}
 };
 
-class LinkedList
-{
-	Node *head;
+class LinkedList {
+  Node* head;
 
-public:
-	LinkedList()
-	{
-		head = NULL;
+ public:
+  LinkedList() { head = NULL; }
+
+  // Add node at begining
+  void prependNode(int data){
+
+	Node* newNode = new Node(data);
+
+	if(head == NULL){
+		head = newNode;
+		return;
 	}
 
-	void prependNode(int data)
-	{
-		Node *newNode = new Node(data);
+	newNode->next = head;
+	head =  newNode;
 
-		if (head == NULL)
-		{
-			head = newNode;
-			return;
-		}
+  }
 
-		newNode->next = this->head;
-		this->head = newNode;
-	}
+  void appendNode(int data);             // Add node at end []
+  void insertNode(int index, int data);  // Insert at given index []
+  void findNode(int data);               // Find by value
+  void getNode(int index);               // Get by index
+  void deleteNode();                     // Delete last node
+  void deleteNodeAt(int index);          // Delete at index
+  void print();                          // Print all nodes [X]
 
-	void appendNode(int data)
-	{
-		Node *newNode = new Node(data);
-		if (head == NULL)
-		{
-			head = newNode;
-			return;
-		}
-		Node *temp = head;
-
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-
-		temp->next = newNode;
-	}
-
-	void insertNode(int index, int data) {}
-	void findNode(int data) {}
-	void getNode(int index) {}
-	void deleteNode() {}
-	void deleteNodeAt(int index) {}
-
-	void print()
-	{
-		Node *temp = head;
-
-		if (head == NULL)
-		{
-			cout << "List is Empty" << endl;
-			return;
-		}
-
-		while (temp != NULL)
-		{
-			cout << temp->data << " -> ";
-			temp = temp->next;
-		}
-	}
+  // ===== Extra common methods =====
+  int length();                   // Return number of nodes
+  bool isEmpty();                 // Check if list is empty
+  void clear();                   // Delete all nodes
+  void reverse();                 // Reverse the list
+  void removeDuplicates();        // Remove duplicate values
+  void sort();                    // Sort list in ascending order
+  void merge(LinkedList& other);  // Merge with another list
+  Node* middleNode();             // Get middle node
+  Node* nthFromEnd(int n);        // Get nth node from end
+  void swapNodes(int x, int y);   // Swap nodes by values
 };
 
-int main()
-{
-	LinkedList list;
-	list.prependNode(10);
-	list.prependNode(20);
-	list.prependNode(30);
-	list.print();
+// we can write like this with scope resolution operator (::)
+void LinkedList::print(){
+	Node* temp = head;
 
-	return 0;
+	while(temp != NULL){
+		cout << temp->data << " -> ";
+		temp = temp -> next;
+	}
+	cout << endl;
+}
+
+// main function
+int main() {
+  LinkedList list;
+
+  list.prependNode(10);
+  list.prependNode(20);
+  list.prependNode(30);
+  list.prependNode(40);
+  list.print();
+
+  return 0;
 }
